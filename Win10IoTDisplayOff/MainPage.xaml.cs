8e5510fcd17e5dfd3f9dd6adcfeeea0578ed71d8
Paul.Farrand <paul.farrand@primetss.com.au>
2019-01-24 14:22:29 +1030

    Added Interop API Call

--- Win10IoTDisplayOff/MainPage.xaml.cs
+++ Win10IoTDisplayOff/MainPage.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.System.Display;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +29,32 @@ namespace Win10IoTDisplayOff
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetMonitorState(MonitorState.OFF);
+        }
+
+        private int SC_MONITORPOWER = 0xF170;
+        private uint WM_SYSCOMMAND = 0x0112;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        enum MonitorState
+        {
+            ON = -1,
+            OFF = 2,
+            STANDBY = 1
+        }
+        private void SetMonitorState(MonitorState state)
+        {
+
+            IntPtr hWnd = new IntPtr(0xFFFF); // Broadcast to all
+
+            SendMessage(hWnd, WM_SYSCOMMAND, (IntPtr)SC_MONITORPOWER, (IntPtr)state);
+
         }
     }
 }
